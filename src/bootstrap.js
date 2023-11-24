c6dfbb4655d8d7d64b5155e14673cf6d290082f7
Camilo Figueroa <cigotete@gmail.com>
2023-11-24 02:06:29 -0500

    Changed bootstrap file to scenario of unkown assigned dom element. Also to be rendered when wanted. Note: Host and all containers may need to be rebooted in order to get the new function.

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,5 +1,32 @@
 import faker from 'faker';
 
-const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
+const mount = (el) => {
+  const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
 
-document.querySelector('#dev-cart').innerHTML = cartText;
+  el.innerHTML = cartText;
+}
+
+// Context/Situation #1
+// We are running this file in development in isolation
+// We are using our local index.html file
+// Which DEFNITELY has an element with an id of 'dev-products
+// We want to immediately render our app into that element
+if (process.env.NODE_ENV === 'development') {
+  const el = document.querySelector('#dev-cart');
+
+  // Assuming our container doesnt have an element
+  // with id 'dev-products'....
+  if (el) {
+    // We are probably running in isolation
+    console.log('Running cart in isolation');
+    mount(el);
+  }
+}
+
+// Context/Situation #2
+// We are running this file in develpment or production
+// through the CONTAINER app
+// NO GUARANTEE that an element with an id of 'dev-products' exists
+// WE DO NOT WANT try to immediately render the app
+console.log('Running cart from container');
+export { mount };
